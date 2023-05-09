86cc4a59bc84187b695933522399a4ec8018c487
Ivan Barba <70910473+IvanBM18@users.noreply.github.com>
2023-05-08 18:23:53 -0600

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,27 @@
+//g++ -std=c++17 a.cpp && a < in.txt > out.txt
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define _ios_base :: sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ENDL '\n'
+#define f first
+#define s second
+#define fore(i,a,b) for(int i = (a), ThxMK = (b); i < ThxMK; ++i)
+#define pb push_back
+#define all(s) begin(s), end(s)
+#define sz(s) int(s.size())
+#define deb(x) cout << #x": " << (x) << endl;
+typedef long double ld;
+typedef long long lli;
+typedef pair<lli,lli> ii;
+typedef vector<lli> vi;
+
+void solve(){
+
+};
+
+int main(){_
+    solve();
+    return 0;
+}
