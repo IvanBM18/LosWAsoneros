335508d743514d9b1c482565257882d79fd6209c
Blackcronw <124933704+Blackcronw@users.noreply.github.com>
2023-05-31 04:51:27 -0600

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,10 +1,8 @@
 //g++ -std=c++17 a.cpp && a < in.txt > out.txt
 //g++ -std=c++11 a.cpp && ./a < in.txt > out.txt
 #include<bits/stdc++.h>
-
 using namespace std;
-
-#define _ ios_base :: sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 #define ENDL '\n'
 #define f first
 #define s second
@@ -13,7 +11,7 @@ using namespace std;
 #define pb push_back
 #define all(s) begin(s), end(s)
 #define sz(s) int(s.size())
-#define deb(x) cout << #x": " << (x) << endl;
+#define deb(x) cout<< #x ": " << (x) << ENDL;
 typedef long double ld;
 typedef long long lli;
 typedef pair<lli,lli> ii;
