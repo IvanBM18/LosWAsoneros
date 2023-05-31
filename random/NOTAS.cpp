08046ff601aef2b0e05dfa4e8b600bb97ef15cfa
Blackcronw <124933704+Blackcronw@users.noreply.github.com>
2023-05-31 05:24:08 -0600

    Update NOTAS.cpp

--- random/NOTAS.cpp
+++ random/NOTAS.cpp
@@ -1,20 +1,3 @@
-//g++ -std=c++17 a.cpp && a < in.txt > out.txt
-#include <bits/stdc++.h>
-#define f first
-#define s second
-#define fore(i,a,b) for(int i = (a), ThxMK = (b); i < ThxMK; ++i)
-#define pb push_back
-#define all(s) begin(s), end(s)
-#define _ ios_base :: sync_with_stdio(0);cin.tie(0);cout.tie(0);
-#define sz(s) int(s.size())
-#define ENDL '\n'
-#define deb(x) cout << #x": " << (x) << endl;
-using namespace std;
-typedef long double ld;
-typedef long long lli;
-typedef pair<lli,lli> ii;
-typedef vector<lli> vi;
-
 //codigo para saber si una strings es un palindromo o no
 void solve(){
     string s;
